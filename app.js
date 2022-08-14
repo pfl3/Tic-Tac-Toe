d5e0706d9846efea16758a22a0ecc7b312be2fb0
pfl3 <tio540@hotmail.com>
2022-08-14 09:31:11 +0900

    add event listener for tile clicks

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const gameBoard = (() => {
-  document.createElement("div");
+// Gameboard module pattern START
+const makeGameBoard = (() => {
+  const tileArr = [];
+  const gameboardDiv = document.querySelector(".gameboard");
+  function makeBoard() {
+    for (let i = 0; i < 9; i++) {
+      const tile = document.createElement("div");
+      tile.setAttribute("tileID", [i]);
+      tile.classList.add("tileStyle", "grid-interact");
+      gameboardDiv.appendChild(tile);
+      tileArr.push(tile[i]);
+      console.log(tile);
+    }
+  }
+  return {
+    tileBoard: makeBoard,
+    tileArray: tileArr,
+  };
 })();
+makeGameBoard.tileBoard();
+document.querySelectorAll(".grid-interact").forEach((tile) => {
+  tile.addEventListener("click", () => {
+    let ID = tile.getAttribute("tileID");
+    tile.classList.add("color");
+    console.log(`You clicked tile ${ID}`);
+  });
+});
+
+// Factory functions START
+
+const playerFactory = function (name) {};
